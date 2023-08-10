5f20a2a9a663b63b68aeb454bede69ea3527e2a5
Cristian González <crisgon1065@gmail.com>
2023-08-09 22:23:07 -0600

    feat: Configuracion inicial de proyecto y plantilla

---
+++ resources/views/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logos/favicon.png') }}"/>
+    <link href="{{ asset('css/styles.min.css') }}" rel="stylesheet">
+    @livewireStyles
+</head>
+
+<body>
+<!-- Body Wrapper -->
+<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+     data-sidebar-position="fixed" data-header-position="fixed">
+    <!-- Sidebar -->
+    @include('partials.sidebar')
+    <!-- Main -->
+    <div class="body-wrapper">
+        @include('partials.header')
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+    </div>
+</div>
+<script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('js/sidebarmenu.js') }}"></script>
+<script src="{{ asset('js/app.min.js') }}"></script>
+<script src="{{ asset('libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+<script src="{{ asset('libs/simplebar/dist/simplebar.js') }}"></script>
+<script src="{{ asset('js/dashboard.js') }}"></script>
+@livewireScripts
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('templates.dashboard');
+})->name('home');
+
+//Rutas para ejemplos templates
+Route::prefix('example')->group(function () {
+    Route::get('buttons', function () {
+        return view('templates.ui-buttons');
+    })->name('buttons');
+
+    Route::get('alerts', function () {
+        return view('templates.ui-alerts');
+    })->name('alerts');
+
+    Route::get('cards', function () {
+        return view('templates.ui-card');
+    })->name('cards');
+
+    Route::get('forms', function () {
+        return view('templates.ui-forms');
+    })->name('forms');
+
+    Route::get('fonts', function () {
+        return view('templates.ui-typography');
+    })->name('fonts');
+
+    Route::get('login', function () {
+        return view('auth.login');
+    })->name('login');
+
+    Route::get('register', function () {
+        return view('auth.register');
+    })->name('register');
+
+    Route::get('icons', function () {
+        return view('templates.icon-tabler');
+    })->name('icons');
+
+    Route::get('sample', function () {
+        return view('templates.sample-page');
+    })->name('sample');
+
+});
